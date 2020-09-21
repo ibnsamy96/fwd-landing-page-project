afec5a13706f22913c147ceadff15d7d6ccf1099
mahmoud-ibn-samy <mahmoudsamy50@outlook.com>
2020-09-21 22:14:34 +0200

    added comments to clarify functions use

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let siteSections = document.querySelectorAll("section");
 
 window.addEventListener("scroll", function () {
+  // event listener to track browser scroll and toggle 'active' class
+
   siteSections = document.querySelectorAll("section");
 
   for (section of siteSections) {
@@ -21,18 +23,31 @@ window.addEventListener("scroll", function () {
   }
 });
 
-console.log("app started");
+// console.log("app started");
 
 const newSections = [];
 
 for (section of siteSections) {
-  console.log(section.dataset.nav);
+  // console.log(section.dataset.nav);
   addSectionToNav(section.dataset.nav, section.id);
 }
 
 document.querySelector(`a[id='header-link']`).classList.add("active");
 
+function addSectionToNav(sectionTitle) {
+  // function that creates all anchor elements for section
+
+  const navMenu = document.querySelector("nav ul");
+  const newMenuItem = document.createElement("li");
+  newMenuItem.innerHTML = `<a  id='${sectionTitle}-link' onclick = "scrollToSection(this)">${sectionTitle.toUpperCase()}</a>`;
+  // console.log(navMenu);
+  // console.log(newMenuItem);
+  navMenu.appendChild(newMenuItem);
+}
+
 const submitNewElement = () => {
+  // function that fires after clicking on 'add section' button and handle that process
+
   const sectionTitle = prompt("Choose section title");
   const idWordsArray = prompt("Choose its special ID").split(" ");
   const sectionContent = prompt("Choose its content");
@@ -55,25 +70,18 @@ const submitNewElement = () => {
     }
 
     addSectionToBody(sectionTitle, sectionID, sectionContent);
-    addSectionToNav(sectionTitle, sectionID);
+    addSectionToNav(sectionTitle);
     handleStoring(sectionTitle, sectionID, sectionContent);
   }
 };
 
-function addSectionToNav(sectionTitle, sectionID) {
-  const navMenu = document.querySelector("nav ul");
-  const newMenuItem = document.createElement("li");
-  newMenuItem.innerHTML = `<a  id='${sectionTitle}-link' onclick = "scrollToSection(this)">${sectionTitle.toUpperCase()}</a>`;
-  console.log(navMenu);
-  console.log(newMenuItem);
-  navMenu.appendChild(newMenuItem);
-}
-
 function addSectionToBody(sectionTitle, sectionID, sectionContent) {
+  // function that adds new sections in the body
+
   newSection = document.createElement("section");
   newSection.id = sectionID;
   newSection.dataset.nav = sectionTitle;
-  console.log(sectionID);
+  // console.log(sectionID);
   newSection.innerHTML = `<h1> ${generateEyeSVG(
     sectionID
   )} ${sectionTitle}</h1><p>${sectionContent}</p>`;
@@ -81,6 +89,8 @@ function addSectionToBody(sectionTitle, sectionID, sectionContent) {
 }
 
 const scrollToSection = (clickedAnchor) => {
+  // function that fires after clicking on any anchor element in the nav and scroll to specified element
+
   const sectionDataNav = clickedAnchor.id.split("-link")[0];
 
   const neededSection = document.querySelector(
@@ -93,6 +103,7 @@ const scrollToSection = (clickedAnchor) => {
 };
 
 const handleStoring = (sectionTitle, sectionID, sectionContent) => {
+  // function to handle storing created sections to local storage
   newSections.push(
     JSON.stringify({
       title: sectionTitle,
@@ -104,15 +115,19 @@ const handleStoring = (sectionTitle, sectionID, sectionContent) => {
 };
 
 const toLocalStorage = () => {
+  // function that stores created sections to local storage
+
   sectionsData = newSections.toString();
   localStorage.setItem("sections", sectionsData);
-  console.log(sectionsData);
-  // console.log(sectionsList);
+  // console.log(sectionsData);
+  // // console.log(sectionsList);
 };
 
 function handleRetrieve() {
+  // function to handle retrieving created sections from local storage
+
   sectionsList = fromLocalStorage();
-  console.log(sectionsList);
+  // console.log(sectionsList);
   if (sectionsList) {
     sectionsList.forEach((element, index) => {
       if (index !== 0) {
@@ -129,6 +144,8 @@ function handleRetrieve() {
 }
 
 const fromLocalStorage = () => {
+  // function to retrieve created sections from local storage
+
   sectionsData = localStorage.getItem("sections");
   if (sectionsData) {
     sectionsList = sectionsData.split(",{");
@@ -142,108 +159,31 @@ const navbar = document.getElementById("navbar");
 navbar.style.display = "flex";
 
 const toggleNav = () => {
+  // function that fires after clicking on toggle button and toggle navbar
+
   if (navbar.style.display === "flex") {
-    console.log("if");
-    console.log(navbar.style.display);
+    // console.log("if");
+    // console.log(navbar.style.display);
     navbar.style.display = "none";
-    console.log(navbar.style.display);
+    // console.log(navbar.style.display);
   } else {
-    console.log("else");
+    // console.log("else");
     navbar.style.display = "flex";
   }
 };
 
-function generateEyeSVG(sectionID) {
-  const svgCode = `<svg
-  onclick="collapseSection('${sectionID}')"
-  class="decollapseIcon"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  version="1.1"
-  width="16"
-  height="16"
-  viewBox="0 0 16 16"
->
-  <path
-    fill="#000"
-    d="M12.9 5.2l-.8.8c1.7.9 2.5 2.3 2.8 3-.7.9-2.8 3.1-7 3.1-.7 0-1.2-.1-1.8-.2l-.8.8c.8.3 1.7.4 2.6.4 5.7 0 8.1-4 8.1-4s-.6-2.4-3.1-3.9z"
-  />
-  <path
-    fill="#000"
-    d="M12 7.1c0-.3 0-.6-.1-.8L7.1 11c.3 0 .6.1.9.1 2.2 0 4-1.8 4-4z"
-  />
-  <path
-    fill="#000"
-    d="M15.3 0l-4.4 4.4C10.1 4.2 9.1 4 8 4 1.3 4 0 9.1 0 9.1s1 1.8 3.3 3L0 15.3v.7h.7L16 .7V0h-.7zM4 11.3C2.4 10.6 1.5 9.5 1.1 9c.3-.7 1.1-2.2 3.1-3.2-.1.4-.2.8-.2 1.3 0 1.1.5 2.2 1.3 2.9L4 11.3zm2.2-3.4l-1 .2s-.3-.5-.3-1.2c0-.8.4-1.5.4-1.5.5-.3 1.3-.3 1.3-.3s-.5.9-.5 1.7c-.1.7.1 1.1.1 1.1z"
-  />
-  <metadata>
-    <rdf:RDF
-      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-      xmlns:dc="http://purl.org/dc/elements/1.1/"
-      ><rdf:Description
-        about="https://iconscout.com/legal#licenses"
-        dc:title="eye,slash"
-        dc:description="eye,slash"
-        dc:publisher="Iconscout"
-        dc:date="2017-09-19"
-        dc:format="image/svg+xml"
-        dc:language="en"
-        ><dc:creator
-          ><rdf:Bag><rdf:li>Vaadin Icons</rdf:li></rdf:Bag></dc:creator
-        ></rdf:Description
-      ></rdf:RDF
-    >
-  </metadata>
-</svg>
-<svg
-  onclick="collapseSection('${sectionID}')"
-  class="collapseIcon"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  version="1.1"
-  width="16"
-  height="16"
-  viewBox="0 0 16 16"
->
-  <path
-    fill="#000"
-    d="M8 3.9C1.3 3.9 0 9 0 9s2.2 4.1 7.9 4.1 8.1-4 8.1-4-1.3-5.2-8-5.2zM5.3 5.4c.5-.3 1.3-.3 1.3-.3s-.5.9-.5 1.6c0 .7.2 1.1.2 1.1L5.2 8s-.3-.5-.3-1.2c0-.8.4-1.4.4-1.4zm2.6 6.7c-4.1 0-6.2-2.3-6.8-3.2.3-.7 1.1-2.2 3.1-3.2-.1.4-.2.8-.2 1.3 0 2.2 1.8 4 4 4s4-1.8 4-4c0-.5-.1-.9-.2-1.3 2 .9 2.8 2.5 3.1 3.2-.7.9-2.8 3.2-7 3.2z"
-  />
-  <metadata>
-    <rdf:RDF
-      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-      xmlns:dc="http://purl.org/dc/elements/1.1/"
-      ><rdf:Description
-        about="https://iconscout.com/legal#licenses"
-        dc:title="eye"
-        dc:description="eye"
-        dc:publisher="Iconscout"
-        dc:date="2017-09-19"
-        dc:format="image/svg+xml"
-        dc:language="en"
-        ><dc:creator
-          ><rdf:Bag><rdf:li>Vaadin Icons</rdf:li></rdf:Bag></dc:creator
-        ></rdf:Description
-      ></rdf:RDF
-    >
-  </metadata>
-</svg>`;
-
-  return svgCode;
-}
-
 const collapseSection = (sectionID, ...params) => {
-  console.log(params);
+  // function that fires after clicking on anchor elements or eye svg element and collapse sections
+
+  // console.log(params);
   section = document.getElementById(sectionID);
   sectionParagraph = document.querySelector("#" + sectionID + " p");
   decollapseIcon = document.querySelector("#" + sectionID + " .decollapseIcon");
   collapseIcon = document.querySelector("#" + sectionID + " .collapseIcon");
-  console.log(sectionParagraph);
+  // console.log(sectionParagraph);
 
   if (sectionParagraph.style.display !== "none" && !params[0]) {
-    console.log("fired");
+    // console.log("fired");
     section.style.paddingTop = "20px";
     section.style.paddingBottom = "20px";
     sectionParagraph.style.display = "none";
@@ -259,3 +199,86 @@ const collapseSection = (sectionID, ...params) => {
     collapseIcon.style.display = "none";
   }
 };
+
+function generateEyeSVG(sectionID) {
+  // function handles creating eye svg element for the new created section
+
+  const svgCode = `<svg
+    onclick="collapseSection('${sectionID}')"
+    class="decollapseIcon"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill="#000"
+      d="M12.9 5.2l-.8.8c1.7.9 2.5 2.3 2.8 3-.7.9-2.8 3.1-7 3.1-.7 0-1.2-.1-1.8-.2l-.8.8c.8.3 1.7.4 2.6.4 5.7 0 8.1-4 8.1-4s-.6-2.4-3.1-3.9z"
+    />
+    <path
+      fill="#000"
+      d="M12 7.1c0-.3 0-.6-.1-.8L7.1 11c.3 0 .6.1.9.1 2.2 0 4-1.8 4-4z"
+    />
+    <path
+      fill="#000"
+      d="M15.3 0l-4.4 4.4C10.1 4.2 9.1 4 8 4 1.3 4 0 9.1 0 9.1s1 1.8 3.3 3L0 15.3v.7h.7L16 .7V0h-.7zM4 11.3C2.4 10.6 1.5 9.5 1.1 9c.3-.7 1.1-2.2 3.1-3.2-.1.4-.2.8-.2 1.3 0 1.1.5 2.2 1.3 2.9L4 11.3zm2.2-3.4l-1 .2s-.3-.5-.3-1.2c0-.8.4-1.5.4-1.5.5-.3 1.3-.3 1.3-.3s-.5.9-.5 1.7c-.1.7.1 1.1.1 1.1z"
+    />
+    <metadata>
+      <rdf:RDF
+        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        ><rdf:Description
+          about="https://iconscout.com/legal#licenses"
+          dc:title="eye,slash"
+          dc:description="eye,slash"
+          dc:publisher="Iconscout"
+          dc:date="2017-09-19"
+          dc:format="image/svg+xml"
+          dc:language="en"
+          ><dc:creator
+            ><rdf:Bag><rdf:li>Vaadin Icons</rdf:li></rdf:Bag></dc:creator
+          ></rdf:Description
+        ></rdf:RDF
+      >
+    </metadata>
+  </svg>
+  <svg
+    onclick="collapseSection('${sectionID}')"
+    class="collapseIcon"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill="#000"
+      d="M8 3.9C1.3 3.9 0 9 0 9s2.2 4.1 7.9 4.1 8.1-4 8.1-4-1.3-5.2-8-5.2zM5.3 5.4c.5-.3 1.3-.3 1.3-.3s-.5.9-.5 1.6c0 .7.2 1.1.2 1.1L5.2 8s-.3-.5-.3-1.2c0-.8.4-1.4.4-1.4zm2.6 6.7c-4.1 0-6.2-2.3-6.8-3.2.3-.7 1.1-2.2 3.1-3.2-.1.4-.2.8-.2 1.3 0 2.2 1.8 4 4 4s4-1.8 4-4c0-.5-.1-.9-.2-1.3 2 .9 2.8 2.5 3.1 3.2-.7.9-2.8 3.2-7 3.2z"
+    />
+    <metadata>
+      <rdf:RDF
+        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        ><rdf:Description
+          about="https://iconscout.com/legal#licenses"
+          dc:title="eye"
+          dc:description="eye"
+          dc:publisher="Iconscout"
+          dc:date="2017-09-19"
+          dc:format="image/svg+xml"
+          dc:language="en"
+          ><dc:creator
+            ><rdf:Bag><rdf:li>Vaadin Icons</rdf:li></rdf:Bag></dc:creator
+          ></rdf:Description
+        ></rdf:RDF
+      >
+    </metadata>
+  </svg>`;
+
+  return svgCode;
+}
